cd395b4d4b5a016d96f2dc94dd56b5e609470719
Cullen <GNRJCULL@ljmu.ac.uk>
2023-04-05 18:33:08 +0100

    initial test of git gui upload

---
+++ mtour.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+    "initialscene": "R0010006",
+    "name": "test",
+    "scenes": [
+        {
+            "faceSize": 2688,
+            "id": "R0010006",
+            "infoHotspots": [
+                {
+                    "icon": "pmicons/winfo.png",
+                    "pitch": -0.07332128188585556,
+                    "rotation": 0,
+                    "text": "ljmu website",
+                    "title": "Back to LJMU",
+                    "yaw": 0.2192657139566778
+                }
+            ],
+            "initialViewParameters": {
+                "fov": 1.2,
+                "pitch": 0,
+                "yaw": 0
+            },
+            "levels": [
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 512,
+                    "tileSize": 256
+                },
+                {
+                    "size": 1024,
+                    "tileSize": 256
+                },
+                {
+                    "size": 2048,
+                    "tileSize": 256
+                }
+            ],
+            "linkHotspots": [
+                {
+                    "icon": "pmicons/wlink000.png",
+                    "initialViewParameters": {
+                        "fov": 1.2,
+                        "pitch": 0,
+                        "yaw": 0
+                    },
+                    "options": {
+                        "transitionDuration": 2000
+                    },
+                    "pitch": 0.04583234616335556,
+                    "rotation": 0,
+                    "target": "R0010007",
+                    "title": "To Lab",
+                    "yaw": -1.6280082198878447
+                }
+            ],
+            "name": "R0010006"
+        },
+        {
+            "faceSize": 2688,
+            "id": "R0010007",
+            "infoHotspots": [
+            ],
+            "initialViewParameters": {
+                "fov": 1.2,
+                "pitch": 0,
+                "yaw": 0
+            },
+            "levels": [
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 512,
+                    "tileSize": 256
+                },
+                {
+                    "size": 1024,
+                    "tileSize": 256
+                },
+                {
+                    "size": 2048,
+                    "tileSize": 256
+                }
+            ],
+            "linkHotspots": [
+                {
+                    "icon": "pmicons/wlink000.png",
+                    "initialViewParameters": {
+                        "fov": 1.2,
+                        "pitch": 0,
+                        "yaw": 0
+                    },
+                    "options": {
+                        "transitionDuration": 2000
+                    },
+                    "pitch": 0.04583234616335556,
+                    "rotation": 0,
+                    "target": "R0010006",
+                    "title": "To Corridor",
+                    "yaw": 1.5683877726319333
+                }
+            ],
+            "name": "R0010007"
+        }
+    ],
+    "settings": {
+        "autorotateEnabled": false,
+        "fullscreenButton": true,
+        "mouseViewMode": "drag",
+        "viewControlButtons": true
+    }
+}
